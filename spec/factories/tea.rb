9c14d8a9af8ae923aa778bb0dc1efd14c3002683
sami-p <sami.peterson14@gmail.com>
2022-01-12 08:52:51 -0700

    Create Tea factories

--- spec/factories/tea.rb
+++ spec/factories/tea.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :tea do
     title {Faker::Tea.variety}
     description {Faker::Tea.type}
-    temperature {Faker::Number.decimal(digits: 3)}
+    temperature {Faker::Number.within(range: 160..212)}
     brew_time {Faker::Number.within(range: 1..5)}
   end
 end
