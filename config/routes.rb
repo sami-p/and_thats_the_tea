00ea7fe7ac7a0b15041f356362781b382cddf718
sami-p <sami.peterson14@gmail.com>
2022-01-13 23:47:28 -0700

    Add show customer route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
        resources :subscriptions, only: [:create, :update]
+       resources :subscription_teas, only: [:create]
+       resources :customers, only: [:show]
     end
   end
 end
