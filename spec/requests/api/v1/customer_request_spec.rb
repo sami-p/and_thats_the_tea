38cb8f8bbd58bdbd5597d4014ec2085f576c1bc9
sami-p <sami.peterson14@gmail.com>
2022-01-13 23:49:34 -0700

    Test show all subscriptions for customer

---
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Customers API" do
+  describe '#show' do
+    it 'lists all subscriptions for a customer' do
+      customer = create(:customer)
+
+      tea_1 = create(:tea)
+      tea_2 = create(:tea)
+      tea_3 = create(:tea)
+      tea_4 = create(:tea)
+      tea_5 = create(:tea)
+      tea_6 = create(:tea)
+
+      subscription_1 = create(:subscription, customer_id: customer.id)
+      subscription_2 = create(:subscription, customer_id: customer.id)
+
+      subscription_tea_1 = SubscriptionTea.create!(subscription_id: subscription_1.id, tea_id: tea_1.id)
+      subscription_tea_2 = SubscriptionTea.create!(subscription_id: subscription_1.id, tea_id: tea_2.id)
+      subscription_tea_3 = SubscriptionTea.create!(subscription_id: subscription_1.id, tea_id: tea_3.id)
+      subscription_tea_4 = SubscriptionTea.create!(subscription_id: subscription_2.id, tea_id: tea_4.id)
+      subscription_tea_5 = SubscriptionTea.create!(subscription_id: subscription_2.id, tea_id: tea_5.id)
+      subscription_tea_6 = SubscriptionTea.create!(subscription_id: subscription_2.id, tea_id: tea_6.id)
+
+      get "/api/v1/customers/#{customer.id}"
+
+      expect(response.status).to eq(200)
+
+      result = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
--- spec/requests/api/v1/subscriptions_request_spec.rb
+++ spec/requests/api/v1/subscriptions_request_spec.rb
@@ -1,48 +1,58 @@
 require 'rails_helper'
 
-describe "Subscriptions API" do
-  it "can create a new subscription" do
-    customer = create(:customer)
+RSpec.describe "Subscriptions API" do
+  describe '#create' do
+    it "can create a new subscription" do
+      customer = create(:customer)
 
-    subscription_params = ({
-                            title: Faker::Hipster.sentence(word_count: 1),
-                            price: 29.99,
-                            status: "active",
-                            frequency: Faker::Subscription.subscription_term,
-                            customer_id: customer.id
-                           })
+      subscription_params = ({
+                              title: Faker::Hipster.sentence(word_count: 1),
+                              price: 29.99,
+                              status: "active",
+                              frequency: Faker::Subscription.subscription_term,
+                              customer_id: customer.id
+                             })
 
-    headers = {"CONTENT_TYPE" => "application/json"}
+      headers = {"CONTENT_TYPE" => "application/json"}
 
-    post "/api/v1/subscriptions", headers: headers, params: JSON.generate(subscription: subscription_params)
+      post "/api/v1/subscriptions", headers: headers, params: JSON.generate(subscription: subscription_params)
 
-    created_subscription = Subscription.last
+      created_subscription = Subscription.last
 
-    expect(response).to be_successful
-    expect(created_subscription.title).to eq(subscription_params[:title])
-    expect(created_subscription.price).to eq(subscription_params[:price])
-    expect(created_subscription.status).to eq(subscription_params[:status])
-    expect(created_subscription.frequency).to eq(subscription_params[:frequency])
+      expect(response).to be_successful
+      expect(created_subscription.title).to eq(subscription_params[:title])
+      expect(created_subscription.price).to eq(subscription_params[:price])
+      expect(created_subscription.status).to eq(subscription_params[:status])
+      expect(created_subscription.frequency).to eq(subscription_params[:frequency])
+    end
   end
 
-  # Add sad path for no existing customer
+  describe '#update' do
+    it "can cancel an existing subscription" do
+      customer = create(:customer)
 
-  it "can cancel an existing subscription" do
-    customer = create(:customer)
-    subscription = create(:subscription).id
-    status = Subscription.last.status
-    subscription_params = ({ status: "cancelled" })
+      tea_1 = create(:tea)
+      tea_2 = create(:tea)
+      tea_3 = create(:tea)
 
-    headers = {"CONTENT_TYPE" => "application/json"}
+      subscription = create(:subscription)
 
-    patch "/api/v1/subscriptions/#{subscription}", headers: headers, params: JSON.generate(subscription: subscription_params)
+      subscription_tea_1 = SubscriptionTea.create!(subscription_id: subscription.id, tea_id: tea_1.id)
+      subscription_tea_2 = SubscriptionTea.create!(subscription_id: subscription.id, tea_id: tea_2.id)
+      subscription_tea_3 = SubscriptionTea.create!(subscription_id: subscription.id, tea_id: tea_3.id)
 
-    cancelled_subscription = Subscription.find_by(id: subscription)
+      status = Subscription.last.status
 
-    expect(response).to be_successful
-    expect(cancelled_subscription.status).to eq("cancelled")
-    # Change frequency based on status ?
+      subscription_params = ({ status: "cancelled" })
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/subscriptions/#{subscription.id}", headers: headers, params: JSON.generate(subscription: subscription_params)
+
+      cancelled_subscription = Subscription.find_by(id: subscription.id)
+
+      expect(response).to be_successful
+      expect(cancelled_subscription.status).to eq("cancelled")
+    end
   end
-
-  # Add sad path for non-valid ID
 end
